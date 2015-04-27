8216ec2223c4ea5af48be3aa4634a172b63125c1
Muhammad Sigit Dude <dudelectric@gmail.com>
2015-04-27 16:12:28 +0700

    second commit

--- assignment.rb
+++ assignment.rb
@@ -1,28 +1,28 @@
 require 'colorize'
 
+#append entire lines into words array
 words = []
-answers = []
-#append entire words into words array
 File.open("dictionary.txt", "r").each_line do |line|
 	words << line
 end
 
 puts "Input random word:"
 print">".yellow
-line = $stdin.readline()
+input = $stdin.readline()
 
 #store available answers in answers array
-words.each do |w|
-	if w.chars.sort == line.chars.sort
-		answers << w
+answers = []
+words.each do |word|
+	if word.chars.sort == input.chars.sort
+		answers << word
     end
 end
 
 #print answers to console
 if answers.size > 0
-	answers.each do |a|
+	answers.each do |answer|
 		print">".green
-		print "Answer: "+ a  
+		print "Answer: "+ answer  
 	end
 else
 	puts "Sorry, no solutions are possible."
